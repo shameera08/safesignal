aa978239590625c58c65e91d56ae2e9acf2cc1af
SHAMEERA  GURUSHEED Z <shameerazafer@gmail.com>
2026-08-22 11:46:23 +0530

    Use environment variable for API base URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 // SafeSignal - src/api/client.js
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
 
 const client = axios.create({ baseURL: API_BASE });
 
